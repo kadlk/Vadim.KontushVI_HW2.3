7859a77825653a35b3435106deda2df9fa6e13fa
Vadim <119fps.com>
2021-10-27 14:02:11 +0300

    git init

---
+++ Vadim.KontushVI_HW2.3/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  Vadim.KontushVI_HW2.3
+//
+//  Created by Vadim Kontush on 27.10.21.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
